ec8107e6529957c7ef28841e3d47261ed02ec9ae
AbhishekS-2099 <abhi.sankar1997@gmail.com>
2019-09-05 10:30:46 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#########################################################
+## File Name: hangman.py                               ##
+## Description: Starter for Hangman project - ICS3U    ##
+#########################################################
+import pygame
+import random
+
+pygame.init()
+winHeight = 480
+winWidth = 700
+win = pygame.display.set_mode((winWidth, winHeight))
+#---------------------------------------#
+# initialize global variables/constants #
+#---------------------------------------#
